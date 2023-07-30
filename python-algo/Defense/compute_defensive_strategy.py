365d75a4c7a47256ab19bad58c4c5911bd35bd37
Matthew Habtezgi <mtarekegn143@gmail.com>
2023-07-29 19:14:18 -0700

    first commit

---
+++ python-algo/Defense/compute_defensive_strategy.py
@@ -0,0 +1,5 @@
+import numpy
+import tensorflow as tf
+
+
+
---
+++ python-algo/map_data_structures.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class Structure:
+    def __init__(self, defenses, offenses):
+        self.defenses = defenses
+        self.offenses = offenses
+    
+    
+    
+    
